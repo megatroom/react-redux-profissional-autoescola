388f1236bded36f7afb1c7c5082aff9235172cd8
Jacob Cabral <jacob-cabral@live.com>
2019-03-14 23:48:22 -0300

    Implementação da interface de edição.

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import classNames from "classnames";
+import uuid from "uuid/v1";
 
 import "./index.scss";
+import { inherits } from "util";
 
 class App extends React.Component {
   state = {
-    students: []
+    students: [
+      { id: 1, name: "Aluna 1" },
+      { id: 2, name: "Aluna 2" },
+      { id: 3, name: "Aluna 3" }
+    ]
   };
 
-  handleAddStudent = name => {
+  handleAdd = name => {
     this.setState(state => ({
-      students: state.students.concat(name)
+      students: state.students.concat({ id: uuid(), name: name })
     }));
+  };
+
+  handleDelete = id => {
+    this.setState(state => {
+      const newStudents = state.students.slice();
+      const index = newStudents.findIndex(student => student.id === id);
+      newStudents.splice(index, 1);
+
+      return {
+        students: newStudents
+      };
+    });
+  };
+
+  handleMove = (direction, index) => {
+    this.setState(state => {
+      const newStudents = state.students.slice();
+      const removedStudent = newStudents.splice(index, 1)[0];
+
+      if (direction === "up") newStudents.splice(index - 1, 0, removedStudent);
+      else newStudents.splice(index + 1, 0, removedStudent);
+
+      return {
+        students: newStudents
+      };
+    });
   };
 
   render() {
     return (
       <div className="container">
-        <NewStudent onAddStudent={this.handleAddStudent} />
-        <StudentList students={this.state.students} />
+        <NewStudent onAdd={this.handleAdd} />
+        <StudentList
+          students={this.state.students}
+          onMove={this.handleMove}
+          onDelete={this.handleDelete}
+        />
       </div>
     );
   }
@@ -30,7 +67,7 @@ class NewStudent extends React.Component {
   };
 
   render() {
-    const { onAddStudent } = this.props;
+    const { onAdd } = this.props;
     const { name } = this.state;
 
     return (
@@ -39,11 +76,11 @@ class NewStudent extends React.Component {
           type="text"
           className="new-student__input"
           placeholder="Digite o nome do novo aluno..."
-          value={this.state.name}
+          value={name}
           onChange={event => this.setState({ name: event.target.value })}
           onKeyPress={event => {
             if (event.key === "Enter") {
-              onAddStudent(event.target.value);
+              onAdd(event.target.value);
               this.setState({ name: "" });
             }
           }}
@@ -53,10 +90,109 @@ class NewStudent extends React.Component {
   }
 }
 
-const StudentList = ({ students }) => (
+class Student extends React.Component {
+  state = {
+    isEditing: false
+  };
+
+  handleEdit = () => this.setState({ isEditing: true });
+
+  handleCancel = () => this.setState({ isEditing: false });
+
+  handleSave = () => {
+    this.props.onEdit(this.props.student.id);
+    this.setState({ isEditing: false });
+  };
+
+  render() {
+    const { student, index, total, onEdit, onDelete, onMove } = this.props;
+    const { isEditing } = this.state;
+
+    return (
+      <div key="student.id" className="student">
+        {isEditing ? (
+          <input
+            type="text"
+            className="student__input"
+            defaultValue={student.name}
+          />
+        ) : (
+          <span className="student__text">{student.name}</span>
+        )}
+        {isEditing && (
+          <React.Fragment>
+            <button
+              className="student__button student__button--red"
+              onClick={this.handleCancel}
+            >
+              <i className="material-icons">clear</i>
+            </button>
+            <button
+              className="student__button student__button--green"
+              onClick={this.handleSave}
+            >
+              <i className="material-icons">done</i>
+            </button>
+          </React.Fragment>
+        )}
+        <button
+          className="student__button"
+          disabled={isEditing}
+          onClick={this.handleEdit}
+        >
+          <i className="material-icons">edit</i>
+        </button>
+        <button
+          className="student__button"
+          disabled={isEditing}
+          onClick={() => {
+            onDelete(student.id);
+          }}
+        >
+          <i className="material-icons">delete</i>
+        </button>
+        <div
+          className={classNames("student__arrows", {
+            "student__arrows--hidden": total === 1
+          })}
+        >
+          <button
+            className={classNames("student__button", "student__button--arrow", {
+              "student__button--hidden": index === 0
+            })}
+            onClick={() => {
+              onMove("up", index);
+            }}
+          >
+            <i className="material-icons">keyboard_arrow_up</i>
+          </button>
+          <button
+            className={classNames("student__button", "student__button--arrow", {
+              "student__button--hidden": index === total - 1
+            })}
+            onClick={() => {
+              onMove("down", index);
+            }}
+          >
+            <i className="material-icons">keyboard_arrow_down</i>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const StudentList = ({ students, onEdit, onDelete, onMove }) => (
   <div className="student-list">
-    {students.map(student => (
-      <div className="student">{student}</div>
+    {students.map((student, index) => (
+      <Student
+        student={student}
+        index={index}
+        total={students.length}
+        onEdit={onEdit}
+        onDelete={onDelete}
+        onMove={onMove}
+      />
     ))}
   </div>
 );
