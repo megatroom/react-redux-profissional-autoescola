ef7807a98e2510acf209f563e23284fb09fe9d22
Jacob Cabral <jacob-cabral@live.com>
2019-04-01 12:30:40 -0300

    Remoção do código de controle da exibição do formulário de cadastro de aluno.

--- src/components/NewStudent.js
+++ src/components/NewStudent.js
@@ -2,8 +2,7 @@ import React from "react";
 
 export default class NewStudent extends React.Component {
   state = {
-    name: "",
-    isAdding: false
+    name: ""
   };
 
   handleAdd(student) {
@@ -16,31 +15,24 @@ export default class NewStudent extends React.Component {
 
     return (
       <div className="new-student">
-        <button className="new-student__button new-student__button--floating">
-          <i className="material-icons">add</i>
+        <input
+          type="text"
+          className="new-student__input"
+          placeholder="Digite o nome do novo aluno..."
+          value={name}
+          onChange={event => this.setState({ name: event.target.value })}
+          onKeyPress={event => {
+            if (event.key === "Enter") this.handleAdd(event.target.value);
+          }}
+        />
+        <button
+          className="new-student__button"
+          onClick={() =>
+            this.handleAdd(document.querySelector(".new-student__input").value)
+          }
+        >
+          <i className="material-icons">save</i>
         </button>
-        <div className="new-student__container">
-          <input
-            type="text"
-            className="new-student__input"
-            placeholder="Digite o nome do novo aluno..."
-            value={name}
-            onChange={event => this.setState({ name: event.target.value })}
-            onKeyPress={event => {
-              if (event.key === "Enter") this.handleAdd(event.target.value);
-            }}
-          />
-          <button
-            className="new-student__button"
-            onClick={() =>
-              this.handleAdd(
-                document.querySelector(".new-student__input").value
-              )
-            }
-          >
-            <i className="material-icons">save</i>
-          </button>
-        </div>
       </div>
     );
   }
