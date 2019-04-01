2cde54d0139ea4cf2ebca690b0e781ddaaf2cd80
Jacob Cabral <jacob-cabral@live.com>
2019-04-01 12:28:53 -0300

    Implementação da alteração do estilo do botão quando o ponteiro do mouse está sobre esse botão.

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,7 +4,7 @@ const Error = ({ onRetry }) => (
   <div className="error">
     <h1>Ops!</h1>
     <p>Ocorreu um erro inesperado ao carregar a lista de estudantes.</p>
-    <div className="error-container">
+    <div className="error__button__background">
       <button className="error__button" onClick={onRetry}>
         Tentar novamente
       </button>
