8e27d23d1c18e7c8b19e131e3998476862b38d37
Jacob Cabral <jacob-cabral@live.com>
2019-04-04 00:47:04 -0300

    Ajuste das classes CSS.

--- src/components/NewTheoryClass.js
+++ src/components/NewTheoryClass.js
@@ -5,8 +5,8 @@ export default class NewTheoryClass extends React.Component {
     name: ""
   };
 
-  handleAdd(student) {
-    this.props.onAdd(student);
+  handleAdd(theoryClass) {
+    this.props.onAdd(theoryClass);
     this.setState({ name: "" });
   }
 
@@ -14,11 +14,11 @@ export default class NewTheoryClass extends React.Component {
     const { name } = this.state;
 
     return (
-      <div className="new-student">
+      <div className="new-theory-class">
         <input
           type="text"
-          className="new-student__input"
-          placeholder="Digite o nome do novo aluno..."
+          className="new-theory-class__input"
+          placeholder="Digite o nome da nova turma..."
           value={name}
           onChange={event => this.setState({ name: event.target.value })}
           onKeyPress={event => {
@@ -26,9 +26,11 @@ export default class NewTheoryClass extends React.Component {
           }}
         />
         <button
-          className="new-student__button"
+          className="new-theory-class__button"
           onClick={() =>
-            this.handleAdd(document.querySelector(".new-student__input").value)
+            this.handleAdd(
+              document.querySelector(".new-theory-class__input").value
+            )
           }
         >
           <i className="material-icons">save</i>
