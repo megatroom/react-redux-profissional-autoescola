6791ed46cf3e5d9224894e36bd968075562e3343
Jacob Cabral <jacob-cabral@live.com>
2019-04-04 00:42:36 -0300

    Ajuste para tornar a mensagem de erro genérica.

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 const Error = ({ onRetry }) => (
   <div className="error">
     <h1>Ops!</h1>
-    <p>Ocorreu um erro inesperado ao carregar a lista de estudantes.</p>
+    <p>Ocorreu um erro inesperado ao carregar os dados.</p>
     <div className="error__button__background">
       <button className="error__button" onClick={onRetry}>
         Tentar novamente
