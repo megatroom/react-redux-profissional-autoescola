be1b656b9d356965fd0d4735e7cc8a5d71540320
Jacob Cabral <jacob-cabral@live.com>
2019-04-26 21:25:41 -0300

    Descrição do propósito desta implementação.

--- src/containers/About.js
+++ src/containers/About.js
@@ -3,6 +3,10 @@ import React from "react";
 const About = () => (
   <div className="about">
     <h1>Autoescola Senna</h1>
+    <h2>Desafio Prático - Parte 1</h2>
+    <p>
+      Implementação dos requisitos da aplicação definidos na Seção 3, Aula 27.
+    </p>
   </div>
 );
 
