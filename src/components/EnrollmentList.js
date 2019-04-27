7fd2811d8d452815e224134a373af3d7e42d088d
Jacob Cabral <jacob-cabral@live.com>
2019-04-26 21:31:06 -0300

    Implementação da regra de listagem de alunos para a inscrição - alunos que não estão em outra turma.

--- src/components/EnrollmentList.js
+++ src/components/EnrollmentList.js
@@ -4,15 +4,19 @@ import Enrollment from "./Enrollment";
 
 const EnrollmentList = ({ students, theoryClass, onEnroll, onUnenroll }) => (
   <div className="enrollment-list">
-    {students.map(student => (
-      <Enrollment
-        key={student.id}
-        student={student}
-        theoryClass={theoryClass}
-        onEnroll={onEnroll}
-        onUnenroll={onUnenroll}
-      />
-    ))}
+    {students.map(
+      student =>
+        (student.theoryClass == null ||
+          student.theoryClass.id == theoryClass.id) && (
+          <Enrollment
+            key={student.id}
+            student={student}
+            theoryClass={theoryClass}
+            onEnroll={onEnroll}
+            onUnenroll={onUnenroll}
+          />
+        )
+    )}
   </div>
 );
 
