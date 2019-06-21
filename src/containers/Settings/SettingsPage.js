b5e8a6ba8431bcef07d513775aa6d1b2ca57bf50
Jacob Cabral <jacob-cabral@live.com>
2019-06-21 10:34:52 -0300

    Ajuste do espaçamento dos botões de seleção do estilo.

--- src/containers/Settings/SettingsPage.js
+++ src/containers/Settings/SettingsPage.js
@@ -10,12 +10,14 @@ const SettingsPage = ({ themes, selectedTheme, toggleTheme }) => (
     <Header>Temas</Header>
     <div className='themes'>
       {themes.map(theme => (
-        <Button key={theme.key} style={theme.style} onClick={() => toggleTheme(theme)}>
-          <p className='themes__item'>
-            {theme.label}
-            {theme.key === selectedTheme.key && <i className='material-icons'>check</i>}
-          </p>
-        </Button>
+        <div key={theme.key} className='themes__box'>
+          <Button style={theme.style} onClick={() => toggleTheme(theme)}>
+            <p className='themes__item'>
+              {theme.label}
+              {theme.key === selectedTheme.key && <i className='material-icons'>check</i>}
+            </p>
+          </Button>
+        </div>
       ))}
     </div>
   </div>
