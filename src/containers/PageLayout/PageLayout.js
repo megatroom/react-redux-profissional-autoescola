c031f2f8061d2206893c0821fd2cecec06b0f684
Jacob Cabral <jacob-cabral@live.com>
2019-06-27 13:12:24 -0300

    Refatoração do consumo das propriedades do contexto do React, para tratar conflitos de nomes.

--- src/containers/PageLayout/PageLayout.js
+++ src/containers/PageLayout/PageLayout.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import withSettings from '../Settings/withSettings';
-import withStudents from '../Students/withStudents';
-import withTheoryClasses from '../TheoryClasses/withTheoryClasses';
-import withEnrollments from '../Enrollments/withEnrollments';
+import StudentsContext from '../Students/StudentsContext';
+import TheoryClassesContext from '../TheoryClasses/TheoryClassesContext';
+import EnrollmentsContext from '../Enrollments/EnrollmentsContext';
 import { AppBar, NavigationDrawer } from '../../components';
 
 import './page-layout.scss';
 
-const PageLayout = ({ children, selectedTheme, menu, isMenuOpen, isLoading, saveHasError, onSaveRetry, onOpenMenu, onCloseMenu }) => (
+const PageLayout = ({ children, selectedTheme, menu, isMenuOpen, onOpenMenu, onCloseMenu }) => (
   <div>
-    <AppBar
-      style={selectedTheme && selectedTheme.style}
-      isLoading={isLoading}
-      saveHasError={saveHasError}
-      onSaveRetry={onSaveRetry}
-      onOpenMenu={onOpenMenu}
-    />
-    <div className='container'>{children}</div>
+    <StudentsContext.Consumer>
+      {({ isLoading: isLoadingStudents, saveHasError: saveStudentsHasError, onSaveRetry: onSaveStudentsRetry }) => (
+        <TheoryClassesContext.Consumer>
+          {({ isLoading: isLoadingTheoryClasses, saveHasError: saveTheoryClassesHasError, onSaveRetry: onSaveTheoryClassesRetry }) => (
+            <Fragment>
+              <AppBar
+                style={selectedTheme && selectedTheme.style}
+                isLoading={isLoadingStudents || isLoadingTheoryClasses}
+                saveHasError={saveStudentsHasError || saveTheoryClassesHasError}
+                onSaveRetry={() => {
+                  saveStudentsHasError && onSaveStudentsRetry();
+                  saveTheoryClassesHasError && onSaveTheoryClassesRetry();
+                }}
+                onOpenMenu={onOpenMenu}
+              />
+              <div className='container'>{children}</div>
+            </Fragment>
+          )}
+        </TheoryClassesContext.Consumer>
+      )}
+    </StudentsContext.Consumer>
     <NavigationDrawer style={selectedTheme && selectedTheme.style} menu={menu} isOpen={isMenuOpen} onCloseMenu={onCloseMenu} />
   </div>
 );
 
-export default withSettings(withStudents(withTheoryClasses(PageLayout)));
+export default withSettings(PageLayout);
