0d2b049c14c3ca2c12058522a58a64293e1e3654
Jacob Cabral <jacob-cabral@live.com>
2019-06-28 20:19:37 -0300

    Utilização do componente de cabeçalho.

--- src/containers/Home/HomePage.js
+++ src/containers/Home/HomePage.js
@@ -2,33 +2,31 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import withSettings from '../Settings/withSettings';
-import { Button } from '../../components';
+import { Button, Header } from '../../components';
 
 import './home.scss';
 
 const HomePage = ({ selectedTheme, history, onCloseMenu }) => (
   <div className='home'>
-    <h2>Serviços</h2>
+    <Header>Serviços</Header>
     <Button
       style={selectedTheme.style}
       onClick={() => {
         onCloseMenu();
         history.push('/theory-classes');
-      }}
-    >
+      }}>
       <span className='home__button'>
         <i className='material-icons'>class</i>
         Aula Teórica
       </span>
     </Button>
-    <h2>Cadastros</h2>
+    <Header>Cadastros</Header>
     <Button
       style={selectedTheme.style}
       onClick={() => {
         onCloseMenu();
         history.push('/students');
-      }}
-    >
+      }}>
       <span className='home__button'>
         <i className='material-icons'>person</i>
         Alunos
