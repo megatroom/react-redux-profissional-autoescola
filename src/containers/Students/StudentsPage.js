8abb92aad73d7b05964619c20fa77aa3c116f91a
Jacob Cabral <jacob-cabral@live.com>
2019-06-28 17:01:53 -0300

    Ajuste da visualização dos componentes de adição de aluno de acordo com a existência ou não de erro de persistência dos dados.

--- src/containers/Students/StudentsPage.js
+++ src/containers/Students/StudentsPage.js
@@ -4,20 +4,33 @@ import withSettings from '../Settings/withSettings';
 import withStudents from './withStudents';
 import { NewStudent, StudentList, Error, FloatButton } from '../../components';
 
-const StudentsPage = ({ selectedTheme, students, isAdding, reloadHasError, onAdd, onSave, onEdit, onDelete, onMove, onRetry }) => {
+const StudentsPage = ({
+  selectedTheme,
+  students,
+  isAdding,
+  reloadHasError,
+  saveHasError,
+  onAdd,
+  onSave,
+  onEdit,
+  onDelete,
+  onMove,
+  onRetry
+}) => {
   if (reloadHasError) return <Error style={selectedTheme && selectedTheme.style} onRetry={onRetry} />;
 
   return (
     <React.Fragment>
       <div className='students__container'>
-        {isAdding ? (
-          <FloatButton icon='clear' style={selectedTheme && selectedTheme.style} onClick={() => onAdd(false)} />
-        ) : (
-          <FloatButton icon='add' style={selectedTheme && selectedTheme.style} onClick={() => onAdd(true)} />
-        )}
+        {!saveHasError &&
+          (isAdding ? (
+            <FloatButton icon='clear' style={selectedTheme && selectedTheme.style} onClick={() => onAdd(false)} />
+          ) : (
+            <FloatButton icon='add' style={selectedTheme && selectedTheme.style} onClick={() => onAdd(true)} />
+          ))}
         <h2>Alunos</h2>
       </div>
-      {isAdding && <NewStudent onAdd={onAdd} onSave={onSave} />}
+      {!saveHasError && isAdding && <NewStudent onAdd={onAdd} onSave={onSave} />}
       <StudentList students={students} onEdit={onEdit} onDelete={onDelete} onMove={onMove} />
     </React.Fragment>
   );
